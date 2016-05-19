5d06279a7a22080b8434fb16fdbdb2c27c3a5ac7
Marlo <MarloB@online.de>
2016-05-19 15:06:35 +0200

    Ich joffe der buggt grade nich rum D:

--- Blatt 5/src/Azubi.java
+++ Blatt 5/src/Azubi.java
@@ -1,31 +1,38 @@
 
-public class Azubi extends Mitarbeiter{
-	//Konstruktor
-public Azubi(int _abteilung, String _vorname, String _nachname, String _wohnort, int _gehalt, int _ausbildungsjahr){
-	super( _abteilung,_vorname,_nachname, _wohnort,_gehalt);
-this.ausbildungsjahr=_ausbildungsjahr;
-}
+public class Azubi extends Mitarbeiter {
+	// Variablen
+	private int ausbildungsjahr;
 
-//Variablen
-private int ausbildungsjahr;
+	// Konstruktor
+	public Azubi(int _abteilung, String _vorname, String _nachname, String _wohnort, int _gehalt,
+			int _ausbildungsjahr) {
+		super(_abteilung, _vorname, _nachname, _wohnort, _gehalt);
+		this.ausbildungsjahr = _ausbildungsjahr;
+	}
 
-// Methoden
-public String  mitarbeiterToString(){
-	return(getVorname()+" "+getName()+":\t "+getWohnort()+", "+getGehalt()+" in Ausbildungsjahr "+ausbildungsjahr+".");
-	
-}
+	// Methoden
+	public String mitarbeiterToString() {
+		return (getVorname() + " " + getName() + ":\t " + getWohnort() + ", " + getGehalt() + " in Ausbildungsjahr "
+				+ ausbildungsjahr + ".");
 
-public String azubidatenToString()	{
-	return (getName()+" in Jahr "+ausbildungsjahr+""
-			+ " bei einem Gehalt von "+getGehalt()+ 
-			"€ in Abteilung "+getAbteilung()); 
-}
+	}
+
+	public int getAusbildungsjahr() {
+		return ausbildungsjahr;
+	}
+
+	public String azubidatenToString() {
+		return (getName() + " in Jahr " + ausbildungsjahr + "" + " bei einem Gehalt von " + getGehalt()
+				+ "€ in Abteilung " + getAbteilung());
+	}
+
 	public static void main(String[] args) {
-		Azubi x1= new Azubi(2, "Martha", "Muster", "Bremen", 210, 2);
+		Azubi x1 = new Azubi(2, "Martha", "Muster", "Bremen", 210, 2);
 
-				System.out.println(x1.mitarbeiterToString());
-				System.out.println(x1.azubidatenToString());
-				x1.generiereAbteilung();
+		System.out.println(x1.mitarbeiterToString());
+		System.out.println(x1.azubidatenToString());
+		System.out.println(x1.getAusbildungsjahr());
+		x1.generiereAbteilung();
 	}
 
 }
--- Blatt 5/src/Vorlesung120516.java
+++
@@ -1,4 +0,0 @@
-
-public class Vorlesung120516 {
-
-}
