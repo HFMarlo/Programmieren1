2bf3497a9f1917a4fba152a0efb519e0657ed8b7
Marlo <MarloB@online.de>
2016-05-20 09:24:30 +0200

    sss

--- Blatt 5/src/Ueberladung.java
+++ Blatt 5/src/Ueberladung.java
@@ -1,29 +1,36 @@
 
+
 public class Ueberladung {
-	//Variablen
+	// Variablen
 	public int Wert1;
 	public int Wert2;
 	public double DWert1;
 	public double DWert2;
-	//Konstruktoren
-	public Ueberladung(int _x1, int _x2){
-		Wert1=_x1;
-		Wert2=_x2;
+
+	// Konstruktoren
+	public Ueberladung(int _x1, int _x2) {
+		Wert1 = _x1;
+		Wert2 = _x2;
 	}
-public Ueberladung(double _x1, double _x2){
-		DWert1=_x1;
-		DWert2=_x2;
+
+	public Ueberladung(double _x1, double _x2) {
+		DWert1 = _x1;
+		DWert2 = _x2;
 	}
-//Methoden
-public int maxInt(){
-	return ( java.lang.Math.max(Wert1,Wert2));
-}
-public double logplusminDouble(){
-	return  (Math.log(java.lang.Math.max(DWert1,DWert2)));
-}
+
+	// Methoden, die kein sicherung brauchen?
+	public int maxInt() {
+		return (java.lang.Math.max(Wert1, Wert2));
+	}
+
+	public double logplusminDouble() {
+		return (Math.log(java.lang.Math.max(DWert1, DWert2)));
+	}
+
 	public static void main(String[] args) {
-		Ueberladung test= new Ueberladung(-4, 333);
-System.out.println(test.logplusminDouble());
+		Ueberladung test = new Ueberladung(-4.3, 333.5);
+		System.out.println(test.logplusminDouble());
+		System.out.println(test.maxInt());
 
 	}
 
